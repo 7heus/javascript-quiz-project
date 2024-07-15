1bcd1af5df7b82830d4d2de8e1936c9ca8efecce
Theus <matheusgoias@hotmail.com>
2024-07-15 17:58:15 +0100

    Day 1 instructions complete Matheus, Prashidhika, Lucas

--- src/quiz.js
+++ src/quiz.js
@@ -29,24 +29,26 @@ class Quiz {
   }
 
   shuffleQuestions() {
-    const questions = this.questions;
+    const questions = this.questions; //Ease of access to questions array
     for (let i = questions.length - 1; i >= 0; i--) {
-      const j = Math.floor(Math.random() * questions.length);
-      const temp = questions[i];
-      questions[i] = questions[j];
-      questions[j] = temp;
+      const j = Math.floor(Math.random() * questions.length); // Rounding down random number between 0 to length of array
+      const temp = questions[i]; // storing question at given index into memory to use in reshuffle
+      questions[i] = questions[j]; // setting question at index to question at random index
+      questions[j] = temp; //so we don't have duplicates, setting stored temp question to the question from random index used previously
     }
     return questions;
   }
 
+  // prettier-ignore
   checkAnswer(answer) {
-    if (answer == this.questions[this.currentQuestionIndex].answer) {
+    if (answer == this.questions[this.currentQuestionIndex]/*getting question from current index*/.answer/*and retrieving the answer stored within*/) {
       this.correctAnswers += 1;
     }
   }
 
   hasEnded() {
-    if (this.currentQuestionIndex < this.questions.length) return false;
-    else if (this.currentQuestionIndex == this.questions.length) return true;
+    if (this.currentQuestionIndex < this.questions.length)
+      return false; // if number at current index is lower than amount of questions then not done
+    else if (this.currentQuestionIndex == this.questions.length) return true; //if both numbers are the same, then has ended
   }
 }
