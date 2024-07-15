46f4f0f4054fcc46bf89006b1fb2e23f2831d5f7
Theus <matheusgoias@hotmail.com>
2024-07-15 17:01:16 +0100

    Day 1 instructions complete Matheus, Prashidhika, Lucas

--- src/question.js
+++ src/question.js
@@ -1,7 +1,24 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (text, choices, answer, difficulty)
 
-    // 2. shuffleChoices()
+  // 2. shuffleChoices()
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
+
+  shuffleChoices() {
+    const choices = this.choices;
+    for (let i = choices.length - 1; i >= 0; i--) {
+      const j = Math.floor(Math.random() * choices.length);
+      const temp = choices[i];
+      choices[i] = choices[j];
+      choices[j] = temp;
+    }
+    return choices;
+  }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,52 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  // 2. getQuestion()
 
-    // 4. shuffleQuestions()
+  // 3. moveToNextQuestion()
 
-    // 5. checkAnswer(answer)
+  // 4. shuffleQuestions()
 
-    // 6. hasEnded()
+  // 5. checkAnswer(answer)
+
+  // 6. hasEnded()
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
+
+  getQuestion() {
+    return this.questions[this.currentQuestionIndex];
+  }
+
+  moveToNextQuestion() {
+    this.currentQuestionIndex += 1;
+  }
+
+  shuffleQuestions() {
+    const questions = this.questions;
+    for (let i = questions.length - 1; i >= 0; i--) {
+      const j = Math.floor(Math.random() * questions.length);
+      const temp = questions[i];
+      questions[i] = questions[j];
+      questions[j] = temp;
+    }
+    return questions;
+  }
+
+  checkAnswer(answer) {
+    if (answer == this.questions[this.currentQuestionIndex].answer) {
+      this.correctAnswers += 1;
+    }
+  }
+
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) return false;
+    else if (this.currentQuestionIndex == this.questions.length) return true;
+  }
 }
